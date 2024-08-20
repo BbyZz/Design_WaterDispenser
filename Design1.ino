ecd6642d7396cdab4f942f174fcae9e591c5fafd
Christian Comia <68741285+BbyZz@users.noreply.github.com>
2024-08-20 23:41:55 +0800

    Design1.ino

--- Design1.ino
+++ Design1.ino
@@ -21,20 +21,6 @@ int ReadCANCEL = 0;
 
 // PLAN TO MOVE COIN SIGNAL TO A PWM ~
 
-byte sensorInterrupt = 0;  // 0 = digital pin 2
-byte sensorPin       = 2;
-
-
-float calibrationFactor = 4.5;
-
-volatile byte pulseCount;  
-
-float flowRate;
-unsigned int flowMilliLitres;
-unsigned long totalMilliLitres;
-
-unsigned long oldTime;
-
 
 
 
@@ -50,17 +36,6 @@ void setup() {
   pinMode(relaySolenoid,  OUTPUT);
   digitalWrite(relaySolenoid, HIGH);
 
-  pinMode(sensorPin, INPUT);
-  digitalWrite(sensorPin, HIGH);
-
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0;
-
-  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-
 }
 
 void loop() {
@@ -72,76 +47,16 @@ void loop() {
 
 
   if (ReadbtnML == HIGH) {
-    delay(100);
     Serial.println("btnML is pressed.");
-    dispenseWater(500);
   } else if (ReadbtnL == HIGH) {
-    delay(100);
     Serial.println("btnL is pressed.");
-    dispenseWater(1000);
   } else if (ReadbtnG == HIGH) {
-    delay(100);
     Serial.println("btnG is pressed.");
-    dispenseWater(18927);
+    digitalWrite(relaySolenoid, LOW);
   } else if (ReadCANCEL == HIGH) {
     Serial.println("Cancel is pressed.");
-    delay(100);
-    digitalWrite(relaySolenoid, LOW);
+    digitalWrite(relaySolenoid, HIGH);
   }
 
 
-}
-
-void dispenseWater(float totalVolume){
-  digitalWrite(relaySolenoid, HIGH);
-  Serial.print("Dispensing: ");
-  Serial.println(totalVolume); 
-
-  while (totalVolume > totalMilliLitres) {
-      if((millis() - oldTime) > 1000)  { // Only process counters once per second  
-
-      detachInterrupt(sensorInterrupt);
-          
-      
-      flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-      
-      
-      oldTime = millis();
-      
-
-      flowMilliLitres = (flowRate / 60) * 1000;
-      
-  
-      totalMilliLitres += flowMilliLitres;
-        
-      unsigned int frac;
-
-      // Print the flow rate for this second in litres / minute
-      Serial.print("Flow rate: ");
-      Serial.print(int(flowRate));  // Print the integer part of the variable
-      Serial.print("L/min");
-      Serial.print("\t"); 		  // Print tab space
-
-      // Print the cumulative total of litres flowed since starting
-      Serial.print("Output Liquid Quantity: ");        
-      Serial.print(totalMilliLitres);
-      Serial.println("mL"); 
-      Serial.print("\t"); 		  // Print tab space
-      Serial.print(totalMilliLitres/1000);
-      Serial.print("L");
-      
-      // Reset the pulse counter so we can start incrementing again
-      pulseCount = 0;
-      
-      // Enable the interrupt again now that we've finished sending output
-      attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-    }
-  }
-  totalMilliLitres = 0;
-}
-
-void pulseCounter()
-{
-  // Increment the pulse counter
-  pulseCount++;
 }
